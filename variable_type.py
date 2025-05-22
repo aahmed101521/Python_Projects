8fb7b7602e7f13a88d5e3f380163e308a64ccd6c
aahmed101521 <abrar.ahmed.du@gmail.com>
2025-05-22 03:43:34 +0200

    new file upload

---
+++ variable_type.py
@@ -0,0 +1,9 @@
+my_integer = 10
+my_float = 3.14
+my_string = "Arsenal till I die!"
+my_boolean = True 
+
+print(my_integer, type(my_integer))
+print(my_float, type(my_float))
+print(my_string, type(my_string))
+print(my_boolean, type(my_boolean))
