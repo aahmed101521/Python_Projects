e9f8a05412c343b4026f6874378e6d48dc3c6c73
aahmed101521 <abrar.ahmed.du@gmail.com>
2025-05-22 04:11:19 +0200

    new commit

---
+++ operator_1.py
@@ -0,0 +1,16 @@
+print("Please provide two inputs!")
+input_1 = input("Enter first input: ")
+input_2 = input("Enter second input: ")
+
+int_1 = int(input_1)
+int_2 = int(input_2)
+
+add = int_1 + int_2 
+sub = int_1 - int_2
+mult = int_1 * int_2
+divide = float(int_1/int_2)
+
+print(add)
+
+
+
