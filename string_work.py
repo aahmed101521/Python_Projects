73e90c4246f229fd55b198a9a2233addfadf5c62
aahmed101521 <abrar.ahmed.du@gmail.com>
2025-05-24 20:21:15 +0200

    new commit

---
+++ string_work.py
@@ -0,0 +1,8 @@
+first_name = str(input("Enetr first name: "))
+last_name = str(input("Enter last name: "))
+age = int(input("Enter your age in numers: "))
+
+
+full_name = first_name + ' ' + last_name
+
+print(f"Your name is {full_name} and you are {age} years old")
