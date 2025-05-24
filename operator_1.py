21f599ce2887eb70745ffdf9275717b67bf432a4
aahmed101521 <abrar.ahmed.du@gmail.com>
2025-05-24 18:43:46 +0200

    new project

--- operator_1.py
+++ operator_1.py
@@ -5,12 +5,24 @@ input_2 = input("Enter second input: ")
 int_1 = int(input_1)
 int_2 = int(input_2)
 
-add = int_1 + int_2 
-sub = int_1 - int_2
-mult = int_1 * int_2
-divide = float(int_1/int_2)
+input_oper = input("Enter '+','-','*' or '/': ")
+int_oper = str(input_oper)
 
-print(add)
+if int_oper == '+':
+    result = int_1 + int_2
+elif int_oper == '-':
+    result = int_1 - int_2
+elif int_oper == '*':
+    result = int_1 * int_2
+elif int_oper == '/':
+    if int_2 != 0:
+        result = int_1 + int_2  
+    else:
+        result = "Zero Error"      
+else:
+    result = "Provide the operator properly"
+    
+print(result)
 
 
 
