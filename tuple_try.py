87d43ffd13e6dfe28e544be1f8d29621570df3a2
aahmed101521 <abrar.ahmed.du@gmail.com>
2025-06-12 21:49:28 +0200

    new commit

---
+++ tuple_try.py
@@ -0,0 +1,16 @@
+# 1. Create a tuple
+my_tuple = (1,"Python",3.14)
+
+# 2. Print the tuple
+print(my_tuple)
+
+# Access and print at index 1
+my_tuple_index = my_tuple[1]
+print(my_tuple_index)
+
+try:
+    my_tuple[0] = 5
+except TypeError as e:
+    print("\nAn attempt to modify the tuple raised an error.")
+    print("Error message:", e)
+    print("This demonstrates that tuples are immutable.")
